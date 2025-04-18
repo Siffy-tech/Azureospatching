387a63d74b066df61342320999409694c8a98813
Safy-Tech <siffyy1@gmail.com>
2025-04-18 13:17:28 +0530

    Updated view logic in views.py

--- myproject/views.py
+++ myproject/views.py
@@ -2,4 +2,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, SAFY! How Are you today.")
+    return HttpResponse("Hello, SAFY! How Are you today, good to have .")
